3c845b46f0a69e89246d1369482225dca19ad9a0
MdTalha008 <142504321+MdTalha008@users.noreply.github.com>
2025-07-10 03:57:14 +0000

    Add mathOperations module and main script for assignment

---
+++ main.js
@@ -0,0 +1,6 @@
+// main.js
+
+import { add as sum, subtract as diff } from './mathOperations.js';
+
+console.log('Sum of 10 and 5 is:', sum(10, 5));
+console.log('Difference of 10 and 5 is:', diff(10, 5));
